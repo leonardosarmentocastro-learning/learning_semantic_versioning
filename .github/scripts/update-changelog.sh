31acdcd42094840b98c2d47e0d43c239712446ab
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2020-12-02 12:41:50 -0300

    BUGFIX SYNC (#22)
    * [PR] RELEASE CANDIDATE (#21)
    
    * feature 1
    
    * 1.1.0
    
    * CHANGELOG.md
    
    * feature/1 github action
    
    * feature/1 github action
    
    * github action
    
    * github action
    
    * 1.2.0
    
    * github action
    
    * 1.3.0
    
    * github action: release-candidate
    
    * 1.4.0
    
    * github action
    
    * 1.5.0
    
    * github action
    
    * CHANGELOG.md
    
    * github action
    
    * github action
    
    * github action
    
    * package.json
    
    * 1.7.0
    
    * CHANGELOG.md
    
    * github action
    
    * 1.8.0
    
    * CHANGELOG.md
    
    * github actoi
    
    * 1.9.0
    
    * github action
    
    * 1.10.0
    
    * CHANGELOG.md
    
    * github action
    
    * 1.11.0
    
    * github action
    
    * github action
    
    * 1.12.0
    
    * CHANGELOG.md
    
    * github action
    
    * github action
    
    * 1.13.0
    
    * CHANGELOG.md
    
    * new feature 15
    
    * 1.14.0
    
    * CHANGELOG.md
    
    * tag and deploy
    
    * feature 17
    
    * 1.14.1
    
    * CHANGELOG.md
    
    * feature/18
    
    * 1.14.2
    
    * CHANGELOG.md
    
    * feature/19
    
    * 1.15.0
    
    * CHANGELOG.md
    
    Co-authored-by: GitHub action <action@github.com>
    
    * 2.0.0
    
    * CHANGELOG.md
    
    Co-authored-by: GitHub action <action@github.com>

---
+++ .github/scripts/update-changelog.sh
@@ -0,0 +1,30 @@
+### This script is responsible for fetching small chunks of information from "git log" instruction,
+### and formatting it cohesive lines which will be added to the beginning of "CHANGELOG.md" file.
+
+readonly local SEMANTIC_VERSION=$(git log -1 --pretty=format:"%s")
+echo "[debug] SEMANTIC_VERSION: $SEMANTIC_VERSION"
+
+readonly local PR_MERGE_DATE=$(git log -2 --format=%ci | head -n 2 | tail -1)
+echo "[debug] PR_MERGE_DATE: $PR_MERGE_DATE"
+
+# https://devhints.io/git-log-format
+# https://stackoverflow.com/questions/14093452/grep-only-the-first-match-and-stop
+readonly local PR_TITLE=$(git log -2 --grep="Merge pull request" --pretty=format:"%b" | head -1)
+echo "[debug] PR_TITLE: $PR_TITLE"
+
+readonly local PR_AUTHOR=$(git log -2 --pretty=format:"%an" --grep="Merge pull request" | head -1)
+echo "[debug] PR_AUTHOR: $PR_AUTHOR"
+
+# https://stackoverflow.com/questions/43428293/git-log-pretty-format-grab-just-a-number-from-the-title
+readonly local PR_NUMBER=$(git log -2 | grep -o '#[0-9]\+' | grep -o '[0-9]\+')
+echo "[debug] PR_NUMBER: $PR_NUMBER"
+
+# Add content to the beginning of "CHANGELOG.md" file.
+# NOTE: Every special character must be back-slashed (escaped).
+echo '' | cat - CHANGELOG.md > temp && mv temp CHANGELOG.md
+echo \* $PR_TITLE \(por \"$PR_AUTHOR\" em \[\#$PR_NUMBER\]\(https\:\/\/github.com\/quero\-edu/melhor\_escola/pull\/$PR_NUMBER\)\) | cat - CHANGELOG.md > temp && mv temp CHANGELOG.md
+echo '' | cat - CHANGELOG.md > temp && mv temp CHANGELOG.md
+echo \#\# $SEMANTIC_VERSION \($PR_MERGE_DATE\) | cat - CHANGELOG.md > temp && mv temp CHANGELOG.md
+
+git add CHANGELOG.md
+git commit -m "CHANGELOG.md"
